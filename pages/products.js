01098f2a04fcd169816c1f581d70e865619db3e7
Jasson Caceres <caceres@MBP-de-Jasson.lan>
2023-01-07 11:57:33 -0500

    verifing bugs

--- pages/products.js
+++ pages/products.js
@@ -10,7 +10,7 @@ export async function getStaticProps() {
 
     return {
         props: { products }, // will be passed to the page component as props
-        revalidate: 5,
+        revalidate: 3,
     }
 }
 
